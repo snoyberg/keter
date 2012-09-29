9e5e4ab23319ac1cd06e66e091286e27c58ec749
Michael Snoyman <michael@snoyman.com>
2012-09-29 22:44:48 +0200

    Added --version

--- main/keter.hs
+++ main/keter.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE NoImplicitPrelude #-}
-import Prelude (error, (++), ($), IO)
+import Prelude (error, (++), ($), IO, putStrLn)
 import System.Environment (getArgs, getProgName)
 import Keter.Main (keter)
+import Paths_keter (version)
+import Data.Version (showVersion)
 
 main :: IO ()
 main = do
     args <- getArgs
     case args of
+        ["--version"] -> putStrLn $ "keter version: " ++ showVersion version
         [dir] -> keter dir
         _ -> do
             pn <- getProgName

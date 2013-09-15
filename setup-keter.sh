3c47a206350db37203e74015981655f9ee9d26cd
Michael Snoyman <michael@snoyman.com>
2013-09-15 10:43:00 +0300

    Updated config file

--- setup-keter.sh
+++ setup-keter.sh
@@ -14,16 +14,30 @@ sudo cp ~/.cabal/bin/keter /opt/keter/bin
 
 sudo mkdir -p /opt/keter/etc
 cat > /tmp/keter-config.yaml <<EOF
-# /opt/keter/etc/keter-config.yaml
+# Directory containing incoming folder, where to store logs, etc. Relative to
+# the config file directory.
 root: ..
-setuid: $USER
-# host: host to bind to
-# port: port to listen on
-# ssl:
-#   host:
-#   port:
-#   key:
-#   certificate:
+
+# Keter can listen on multiple ports for incoming connections. These ports can
+# have HTTPS either enabled or disabled.
+listeners:
+    # HTTP
+    - host: "*4" # Listen on all IPv4 hosts
+      #port: 80 # Could be used to modify port
+    # HTTPS
+    - host: "*4"
+      #port: 443
+      key: key.pem
+      certificate: certificate.pem
+
+# User to run applications as
+
+# setuid: ubuntu
+
+# Get the user's IP address from x-forwarded-for. Useful when sitting behind a
+# load balancer like Amazon ELB.
+
+# ip-from-header: true
 EOF
 sudo chown root:root /tmp/keter-config.yaml
 sudo mv /tmp/keter-config.yaml /opt/keter/etc

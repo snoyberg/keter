121bf5bffda6ce0de4a53d554dc72076d4742d42
Christopher Reichert <creichert07@gmail.com>
2015-01-08 23:42:39 -0600

    Remove obsolete tests.

--- test/logfile.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE OverloadedStrings #-}
-import Keter.LogFile
-import Keter.Prelude
-
-main = runKIO print $ do
-    Right lf <- start "log/test"
-    addChunk lf "foo\n"
-    addChunk lf "bar\n"
-    addChunk lf "baz\n"
--- test/process-torun.hs
+++
@@ -1,7 +0,0 @@
-import Control.Concurrent
-import Data.Time
-
-main = do
-    now <- getCurrentTime
-    writeFile "process.txt" $ show now
-    threadDelay 1000000

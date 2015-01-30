51247cc3d96ca9c10af823e96e928f49b79d8a24
Marcin Tolysz <tolysz@gmail.com>
2015-01-30 21:19:36 +0000

    Update assert to match the correct case

--- test/LabelMapSpec.hs
+++ test/LabelMapSpec.hs
@@ -41,17 +41,19 @@ caseSubdomainIntegrity = do
   let test0 = LM.empty
       test1 = LM.insert "someapp.com"         () test0
       test2 = LM.insert "api.someapp.com"     () test1
-      test3 = LM.insert "ipa.someapp.com"     () test2
+      test3a = LM.delete "someapp.com"           test2
+      test3b = LM.insert "api.someapp.com"    () test0 -- case from the bug report
+
+  let test3 = LM.insert "ipa.someapp.com"     () test2
       test4 = LM.insert "bla.api.someapp.com" () test3
       test5 = LM.delete "someapp.com"            test4
       test6 = LM.delete "ipa.someapp.com"        test5
       test7 = LM.delete "api.someapp.com"        test6
       test8 = LM.delete "bla.api.someapp.com"    test7
       test9 = LM.delete "bla.api.someapp.com"    test4
-      testb = LM.insert "api.someapp.com"     () test0
       msg = "Subdomains inserted and deleted between bundles"
 
-  print test5
-  print test7
+  print test3a
+  print test3b
 
-  assertBool msg $ test5 == test7
+  assertBool msg $ test3a == test3b

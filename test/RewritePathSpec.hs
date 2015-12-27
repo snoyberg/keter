59cc893aa7e5d4a6f3fea26a3239919304d25305
vlatkoB <vlatko.basic@gmail.com>
2015-12-27 12:32:11 +0100

    Add ability to rewrite incoming URLs

---
+++ test/RewritePathSpec.hs
@@ -0,0 +1,62 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module RewritePathSpec where
+
+-- import qualified Data.IORef     as I
+import           Test.Hspec
+-- import           Test.HUnit
+
+import Network.HTTP.ReverseProxy.Rewrite
+import           Network.URI                 (URI (..), URIAuth (..), nullURI,
+                                              parseRelativeReference)
+import Data.Monoid
+-- import Control.Applicative
+
+site :: String
+site = "subdomain.host.com"
+
+defURI :: URI
+defURI = nullURI{uriAuthority = Just $ URIAuth "" site "",
+                 uriPath      = "",
+                 uriQuery     = ""}
+
+spec :: Spec
+spec = do
+  describe "Merging Queries" $ do
+    it "merges two queries" $ do
+      let origQS = nullURI{uriQuery="?qs1=1&chngQ=origValue"}
+          rewQS  = nullURI{uriQuery="?qs2=3&qs3=&chngQ=rewValue"}
+      uriQuery (mergeQueries origQS rewQS) `shouldBe` "?chngQ=rewValue&qs1=1&qs2=3&qs3="
+
+  describe "Rewrite Path" $ do
+    it "matches Host, Path and Query" $ do
+      let url =                                    "//" <> site <> "/?query=qValue1&another=qValue2"
+          rp  = RewritePath HostPathQuery (read $ "^//" <> site <> "(/?)\\?query=(.*)&another=(.*)")
+                                                  ("//" <> site <> "/abc?query=$2&another=$3")
+      (parseRelativeReference url >>= rewritePathRule [rp])
+         `shouldBe` Just defURI{ uriPath  = "/abc",
+                                 uriQuery ="?another=qValue2&query=qValue1"}
+
+    it "matches Host, Path and Query with subdomain prefix" $ do
+      let url =                                    "//part-" <> site <> "/?query=qValue1"
+          rp  = RewritePath HostPathQuery (read $ "^//(.*)-" <> site <> "(/?)\\?query=(.*)")
+                                                      ( "//" <> site <> "/abc?domain=$1&query=$3")
+      (parseRelativeReference url >>= rewritePathRule [rp])
+         `shouldBe` Just defURI{ uriPath  = "/abc",
+                                 uriQuery ="?domain=part&query=qValue1"}
+
+    it "matches Path and Query" $ do
+      let url =                    "/path1/more_path/?query=qValue1"
+          rp  = RewritePath PathQuery (read "^/(.*)/\\?query=(.*)")
+                                            "/$1/abc?query=$2"
+      (parseRelativeReference url >>= rewritePathRule [rp])
+         `shouldBe` Just nullURI{ uriPath  = "/path1/more_path/abc",
+                                  uriQuery ="?query=qValue1"}
+
+    it "matches Path and Query, rewrite part to query" $ do
+      let url =                            "/path1/more_fruit/rest"
+          rp  = RewritePath PathOnly (read "^/(.*)/more_(.*)/(.*)")
+                                           "/$1/move-to-query/$3/?query=$2"
+
+      (parseRelativeReference url >>= rewritePathRule [rp])
+         `shouldBe` Just nullURI{ uriPath  = "/path1/move-to-query/rest/?query=fruit"}

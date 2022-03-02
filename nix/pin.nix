9457e26a756dbd7bd356796166b2d121f8af5c70
Jappie Klooster <jappieklooster@hotmail.com>
2022-03-02 15:07:24 +0100

    Add nix shell
    This provides a basic nix shell with cabal
    and a pin.
    It's pretty much cabal2nix . --shell

---
+++ nix/pin.nix
@@ -0,0 +1,6 @@
+import (builtins.fetchGit {
+    # Descriptive name to make the store path easier to identify
+    name = "nixos-pin-2022.03.02";
+    url = "https://github.com/nixos/nixpkgs/";
+    rev = "5d5de3be19855371eecbea5fd500818925ec109d";
+    })

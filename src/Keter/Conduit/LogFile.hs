194015e2e69c2178e3f16fe03d7495e4bfd1da21
Riuga <riuga@tuta.io>
2023-03-29 07:58:19 -0500

    Update `RotatingLog` to wrap a FastLogger and its release callback

--- src/Keter/Conduit/LogFile.hs
+++ src/Keter/Conduit/LogFile.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
 module Keter.Conduit.LogFile
-    ( RotatingLog
+    ( RotatingLog(..)
+    , rotationSpec
     , openRotatingLog
     , addChunk
     , close
     , defaultMaxTotal
+    , defaultBufferSize
     ) where
 
 import           Control.Concurrent             (forkIO)
@@ -23,6 +26,7 @@ import           System.FilePath                ((<.>), (</>))
 import qualified System.IO                      as SIO
 import           System.IO.Unsafe               (unsafePerformIO)
 import           System.Mem.Weak                (addFinalizer)
+import qualified System.Log.FastLogger          as FL
 
 data Command = AddChunk !S.ByteString
              | Close
@@ -30,104 +34,17 @@ data Command = AddChunk !S.ByteString
 -- | Represents a folder used for totating log files.
 --
 -- Since 0.2.1
-data RotatingLog = RotatingLog !(TVar State)
--- Use a data instead of a newtype so that we can attach a finalizer.
+data RotatingLog = RotatingLog
+  { rlLog :: forall a. FL.ToLogStr a => a -> IO ()
+  , rlClose :: IO () 
+  }
 
-data State = Closed
-           | Running !SIO.Handle !(TBQueue Command)
+rotationSpec :: FilePath -> FL.FileLogSpec
+rotationSpec dir =
+    FL.FileLogSpec dir defaultMaxTotal maxBound -- TODO: do we want to overwrite logs after a certain point? leaving this INT_MAX for now
 
-queue :: Command -> RotatingLog -> IO ()
-queue cmd (RotatingLog ts) = atomically $ do
-    s <- readTVar ts
-    case s of
-        Closed -> return ()
-        Running _ q -> writeTBQueue q cmd
-
-addChunk :: RotatingLog -> S.ByteString -> IO ()
-addChunk lf bs = queue (AddChunk bs) lf
-
-close :: RotatingLog -> IO ()
-close = queue Close
-
--- | Create a new @RotatingLog@.
---
--- Since 0.2.1
-openRotatingLog :: FilePath -- ^ folder to contain logs
-                -> Word -- ^ maximum log file size, in bytes
-                -> IO RotatingLog
-openRotatingLog dir maxTotal = do
-    createDirectoryIfMissing True dir
-    bracketOnError (moveCurrent dir) SIO.hClose $ \handle -> do
-        queue' <- newTBQueueIO 5
-        let s = Running handle queue'
-        ts <- newTVarIO s
-        void $ forkIO $ loop dir ts maxTotal
-        let rl = RotatingLog ts
-        addFinalizer rl (atomically (writeTBQueue queue' Close))
-        return rl
-
-current :: FilePath -- ^ folder containing logs
-        -> FilePath
-current = (</> "current.log")
-
-moveCurrent :: FilePath -- ^ folder containing logs
-            -> IO SIO.Handle -- ^ new handle
-moveCurrent dir = do
-    let curr = current dir
-    x <- doesFileExist curr
-    when x $ do
-        now <- getCurrentTime
-        renameFile curr $ dir </> suffix now
-    SIO.openFile curr SIO.WriteMode
-
-suffix :: UTCTime -> FilePath
-suffix now =
-    (concatMap fix $ takeWhile (/= '.') $ show now) <.> "log"
-  where
-    fix ' ' = "_"
-    fix c | '0' <= c && c <= '9' = [c]
-    fix _ = ""
-
-loop :: FilePath -- ^ folder containing logs
-     -> TVar State
-     -> Word -- ^ maximum total log size
-     -> IO ()
-loop dir ts maxTotal =
-    go 0 `finally` (closeCurrentHandle `finally` atomically (writeTVar ts Closed))
-  where
-    closeCurrentHandle = bracket
-        (atomically $ do
-            s <- readTVar ts
-            case s of
-                Closed -> return Nothing
-                Running h _ -> return $! Just h)
-        (maybe (return ()) SIO.hClose)
-        (const $ return ())
-
-    go total = do
-        res <- atomically $ do
-            s <- readTVar ts
-            case s of
-                Closed -> return Nothing
-                Running handle queue' -> do
-                    cmd <- readTBQueue queue'
-                    case cmd of
-                        Close -> return Nothing
-                        AddChunk bs -> return $! Just (handle, queue', bs)
-        case res of
-            Nothing -> return ()
-            Just (handle, queue', bs) -> do
-                let total' = total + fromIntegral (S.length bs)
-                S.hPut handle bs
-                SIO.hFlush handle
-                if total' > maxTotal
-                    then do
-                        bracket
-                            (SIO.hClose handle >> moveCurrent dir)
-                            (\handle' -> atomically $ writeTVar ts $ Running handle' queue')
-                            (const $ return ())
-                        go 0
-                    else go total'
-
-defaultMaxTotal :: Word
+defaultMaxTotal :: Integer
 defaultMaxTotal = 5 * 1024 * 1024 -- 5 MB
+
+defaultBufferSize :: Int
+defaultBufferSize = 256 -- TODO: Reasonable value?

3df39a38b0b7288d49619c964b7e875db806e3b8
Stanislav Smirnov <stanislav.ktak.smirnov@gmail.com>
2025-07-18 01:21:24 +0300

    Test for reloading stuck on crash-looping bundles
    Issue #294

--- vm.nix
+++ vm.nix
@@ -5,48 +5,65 @@ testers.nixosTest {
 
   nodes.server = { pkgs, ... }: {
 
-    environment = {
-      etc = {
+    environment =
+      let
+        nc_sh = { signature ? "", bad ? false }: {
+          text =
+            let
+              content = ''
+                Hello, world!
+                ${signature}
+              '';
+              length = builtins.stringLength content;
+            in
+            ''
+              #!/bin/sh
+              ${if bad then "exit 1" else ""}
+              while true; do
+                ${pkgs.netcat}/bin/nc -l "$PORT" <<'EOF'
+              HTTP/1.1 200 OK
+              Content-Length: ${builtins.toString length}
+              Connection: close
 
-        "nc.sh" = {
-          text = ''
-            #!/bin/sh
-            while true; do
-              ${pkgs.netcat}/bin/nc -l "$PORT" <<'EOF'
-            HTTP/1.1 200 OK
-            Content-Length: 13
-            Connection: close
-
-            Hello, world!
-            EOF
-            done
-          '';
+              ${content}
+              EOF
+              done
+            '';
           mode = "0755";
         };
 
-        "keter.yaml".text = ''
-          stanzas:
-            - type: webapp
-              exec: ../nc.sh
-              hosts:
-                - localhost
-        '';
-
-        "deploy.sh" = {
+        deploy_sh = { num ? "" }: {
           text = ''
             mkdir -p /tmp/bundle/config
-            cp /etc/nc.sh /tmp/bundle/nc.sh
+            cp /etc/nc${num}.sh /tmp/bundle/nc.sh
             cp /etc/keter.yaml /tmp/bundle/config/keter.yaml
             tar -C /tmp/bundle -czvf /tmp/nc.keter .
-            cp /tmp/nc.keter /opt/keter/incoming
+            cp /tmp/nc.keter /opt/keter/incoming/
           '';
           mode = "0755";
         };
+      in
+      {
+        etc = {
+          "nc.sh" = nc_sh { signature = "Test1!"; };
+          "nc2.sh" = nc_sh { signature = "Test2!"; bad = true; };
+          "nc3.sh" = nc_sh { signature = "Test3!"; };
 
+          "keter.yaml".text = ''
+            stanzas:
+              - type: webapp
+                exec: ../nc.sh
+                hosts:
+                  - localhost
+          '';
+
+          "deploy.sh" = deploy_sh { };
+          "deploy2.sh" = deploy_sh { num = "2"; };
+          "deploy3.sh" = deploy_sh { num = "3"; };
+        };
+
+        systemPackages = [ pkgs.netcat ];
       };
-
-      systemPackages = [ pkgs.netcat ];
-    };
 
     imports = [
       ./keter.nix
@@ -82,5 +99,15 @@ testers.nixosTest {
     server.succeed("touch /opt/keter/incoming/nc.keter")
     server.sleep(10)
     server.succeed("! grep -q 'file is locked' /opt/keter/log/keter.log")
+
+    # Crash-looping bundles should not get reloading stuck
+    # https://github.com/snoyberg/keter/issues/294
+    server.succeed("curl localhost | grep 'Test1!'") # Previosly deployed script is OK
+    server.succeed(". /etc/deploy2.sh") # Load the crashed script
+    server.sleep(10)
+    server.succeed("curl localhost | grep 'Test1!'") # bad script doesn't break the good one
+    server.succeed(". /etc/deploy3.sh") # Load new good script
+    server.sleep(10)
+    server.succeed("curl localhost | grep 'Test3!'") # bad script is unloaded and doesn't stuck
   '';
 }
